5851a74727ff14eedf034c02661b1d30a1640d89
LukaszBomba <101752120+LukaszBomba@users.noreply.github.com>
2022-09-05 17:47:59 +0200

    Update
    Style

--- src/index.js
+++ src/index.js
@@ -1,45 +1,54 @@
 import './sass/index.scss';
-import { imageGet } from './js/imageGet';
-import { buildGallery } from './js/buildGallery'
+import { fetchPhoto } from './js/fetchPhoto';
+import { pictureGallery } from './js/pictureGallery';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
 let page = 1;
 const query = document.querySelector('.header_form-input');
 const button = document.querySelector('.header_form-submit');
 const lightbox = new SimpleLightbox('.gallery a');
 
+button.addEventListener('click', event => {
+  event.preventDefault();
+  fetchPhoto(query.value, page)
+    .then(({ data }) => {
+      document.querySelector('.gallery').innerHTML = '';
+      page = 1;
+      if (data.totalHits == 0) {
+        return Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+      return (
+        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`),
+        pictureGallery(data.hits),
+        lightbox.refresh()
+      );
+    })
+    .catch(error => {
+      Notiflix.Notify.failure(error);
+    });
+});
 
-
-button.addEventListener('click', (event) => {
-    event.preventDefault();
-    imageGet(query.value,page)
+// Add photos on scroll
+window.addEventListener('scroll', () => {
+  console.log(window.scrollY); //scroll
+  console.log(window.innerHeight);
+  if (
+    window.scrollY + window.innerHeight >=
+    document.documentElement.scrollHeight
+  ) {
+    page++,
+      fetchPhoto(query.value, page)
         .then(({ data }) => {
-            document.querySelector('.gallery').innerHTML = '';
-            page = 1;
-            if (data.totalHits == 0) {return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.') }
-            return Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`),
-            buildGallery(data.hits),
-            lightbox.refresh();
+          return pictureGallery(data.hits), lightbox.refresh();
         })
-        .catch(error => { Notiflix.Notify.failure(error) })
-})
-
-query.addEventListener('click', (e) => { return query.value = '' })
-
-
-window.addEventListener('scroll',()=>{
-    console.log(window.scrollY) //scrolled from top
-    console.log(window.innerHeight) //visible part of screen
-    if(window.scrollY + window.innerHeight >= 
-    document.documentElement.scrollHeight){
-        page++,
-        imageGet(query.value,page)
-            .then(({ data }) => {
-            return buildGallery(data.hits),
-            lightbox.refresh();
-        })
-            .catch(() => {Notiflix.Notify.failure(`We're sorry, but you've reached the end of search results.`)
-            })
-    }
-})
+        .catch(() => {
+          Notiflix.Notify.failure(
+            `We're sorry, but you've reached the end of search results.`
+          );
+        });
+  }
+});
---
+++ src/js/pictureGallery.js
@@ -0,0 +1,32 @@
+const gallery = document.querySelector('.gallery');
+let pictureGallery = data => {
+  const markup = data
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+        <a href="${largeImageURL}"> <img src="${webformatURL}" alt="${tags}" loading="lazy" title=""/></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>${likes}</p>
+    <p class="info-item">
+      <b>Views</b>${views}</p>
+    <p class="info-item">
+      <b>Comments</b>${comments}</p>
+    <p class="info-item">
+      <b>Downloads</b>${downloads}</p>
+  </div></div>
+`;
+      }
+    )
+    .join('');
+  return gallery.insertAdjacentHTML('beforeend', markup);
+};
+export { pictureGallery };
